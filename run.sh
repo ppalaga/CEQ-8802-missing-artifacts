f76c109311959df226cdd92c3a8b0f6f198a2857
Peter Palaga <ppalaga@redhat.com>
2024-08-16 16:24:13 +0200

    Helisaw work

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 
 LOCAL_REPO=~/zzz/test-repo
-SETTINGS=~/orgs/ceq-3.8/reproducers/CEQ-8802-missing-artifacts/settings.xml
+SETTINGS=~/orgs/cq-prod-3.8/reproducers/CEQ-8802-missing-artifacts/settings.xml
 
 mkdir -p $LOCAL_REPO
 
@@ -16,6 +16,7 @@ find $LOCAL_REPO -type d -name '*redhat-*' | xargs rm -Rf
 QUARKUS_MRRC_REPOSITORY=~/zzz/rh-quarkus-platform-3.8.5.GA-maven-repository/maven-repository
 CEQ_MRRC_REPOSITORY=$QUARKUS_MRRC_REPOSITORY
 #~/zzz/rhaf-camel-4.4.0-for-quarkus-3.8.0.CR5-maven-repository/maven-repository
+INDY_BASE_URL=https://indy.psi.redhat.com/api/content/maven/group/static
 
 # get the versions from the MRRC repos
 QUARKUS_BOM_GROUP_ID="com.redhat.quarkus.platform"
@@ -30,24 +31,19 @@ CAMEL_VERSION=$(ls $CEQ_MRRC_REPOSITORY/org/apache/camel/camel-direct)
 #git clone -b 3.8.0-product -o midstream https://github.com/jboss-fuse/camel-quarkus.git
 #cd camel-quarkus
 
-# Install the missing Jetty BOM
-JETTY_VERSION=$(ls $CEQ_MRRC_REPOSITORY/org/eclipse/jetty/jetty-core)
-mkdir -p $LOCAL_REPO/org/eclipse/jetty/jetty-bom/$JETTY_VERSION
-
-MRRC_BASE_URL="https://maven.repository.redhat.com/earlyaccess/all"
-curl $MRRC_BASE_URL/org/eclipse/jetty/jetty-bom/$JETTY_VERSION/jetty-bom-$JETTY_VERSION.pom > $LOCAL_REPO/org/eclipse/jetty/jetty-bom/$JETTY_VERSION/jetty-bom-$JETTY_VERSION.pom
-curl $MRRC_BASE_URL/org/eclipse/jetty/jetty-bom/$JETTY_VERSION/jetty-bom-$JETTY_VERSION.pom.md5 > $LOCAL_REPO/org/eclipse/jetty/jetty-bom/$JETTY_VERSION/jetty-bom-$JETTY_VERSION.pom.md5
-curl $MRRC_BASE_URL/org/eclipse/jetty/jetty-bom/$JETTY_VERSION/jetty-bom-$JETTY_VERSION.pom.sha1 > $LOCAL_REPO/org/eclipse/jetty/jetty-bom/$JETTY_VERSION/jetty-bom-$JETTY_VERSION.pom.sha1
+# Put the flattened BOM instead of the BOM with imports (some of which (such as jetty-bom) are missing in MRRC.zip)
+cp poms/bom/src/main/generated/flattened-reduced-pom.xml poms/bom/pom.xml
 
 # Install the missing quarkus-test-artemis arifact from Indy
-INDY_BASE_URL=https://indy.psi.redhat.com/api/content/maven/group/static
 QUARKUS_ARTEMIS_VERSION=$(ls $CEQ_MRRC_REPOSITORY/io/quarkiverse/artemis/quarkus-artemis-jms)
 ARTEMIS_VERSION=$(ls $CEQ_MRRC_REPOSITORY/org/apache/activemq/artemis-commons)
 # strip the redhat suffix
 ARTEMIS_VERSION=$(echo $ARTEMIS_VERSION | sed 's|[-\\.]redhat-.*||')
-mkdir -p $LOCAL_REPO/io/quarkiverse/artemis/quarkus-test-artemis/$QUARKUS_ARTEMIS_VERSION
-curl $INDY_BASE_URL/io/quarkiverse/artemis/quarkus-test-artemis/$QUARKUS_ARTEMIS_VERSION/quarkus-test-artemis-$QUARKUS_ARTEMIS_VERSION.jar > $LOCAL_REPO/io/quarkiverse/artemis/quarkus-test-artemis/$QUARKUS_ARTEMIS_VERSION/quarkus-test-artemis-$QUARKUS_ARTEMIS_VERSION.jar
-curl $INDY_BASE_URL/io/quarkiverse/artemis/quarkus-test-artemis/$QUARKUS_ARTEMIS_VERSION/quarkus-test-artemis-$QUARKUS_ARTEMIS_VERSION.pom > $LOCAL_REPO/io/quarkiverse/artemis/quarkus-test-artemis/$QUARKUS_ARTEMIS_VERSION/quarkus-test-artemis-$QUARKUS_ARTEMIS_VERSION.pom
+if [ ! -f "$LOCAL_REPO/io/quarkiverse/artemis/quarkus-test-artemis/$QUARKUS_ARTEMIS_VERSION/quarkus-test-artemis-$QUARKUS_ARTEMIS_VERSION.jar" ]; then
+    mkdir -p $LOCAL_REPO/io/quarkiverse/artemis/quarkus-test-artemis/$QUARKUS_ARTEMIS_VERSION
+    curl $INDY_BASE_URL/io/quarkiverse/artemis/quarkus-test-artemis/$QUARKUS_ARTEMIS_VERSION/quarkus-test-artemis-$QUARKUS_ARTEMIS_VERSION.jar > $LOCAL_REPO/io/quarkiverse/artemis/quarkus-test-artemis/$QUARKUS_ARTEMIS_VERSION/quarkus-test-artemis-$QUARKUS_ARTEMIS_VERSION.jar
+    curl $INDY_BASE_URL/io/quarkiverse/artemis/quarkus-test-artemis/$QUARKUS_ARTEMIS_VERSION/quarkus-test-artemis-$QUARKUS_ARTEMIS_VERSION.pom > $LOCAL_REPO/io/quarkiverse/artemis/quarkus-test-artemis/$QUARKUS_ARTEMIS_VERSION/quarkus-test-artemis-$QUARKUS_ARTEMIS_VERSION.pom
+fi
 # Replace quarkiverse-artemis.version in the top pom.xml
 echo -e "cd /*[local-name() = 'project']//*[local-name() = 'properties']//*[local-name() = 'quarkiverse-artemis.version']\n cat text()\n set $QUARKUS_ARTEMIS_VERSION\n cat text()\n save\n bye" | xmllint --shell pom.xml
 # Force some Artemis community versions in the test BOM
@@ -56,6 +52,14 @@ if grep -q "<artifactId>artemis-server</artifactId>" poms/bom-test/pom.xml; then
 else
   echo "Adding artemis-server constraint to poms/bom-test/pom.xml"
   sed -i "s|        </dependencies>|            <dependency>\n                <groupId>org.apache.activemq</groupId>\n                <artifactId>artemis-server</artifactId>\n                <version>${ARTEMIS_VERSION}</version>\n            </dependency>\n            <dependency>\n                <groupId>org.apache.activemq</groupId>\n                <artifactId>artemis-amqp-protocol</artifactId>\n                <version>${ARTEMIS_VERSION}</version>\n            </dependency>\n        </dependencies>|" poms/bom-test/pom.xml
+fi
+
+# Install the missing org.mapstruct:mapstruct-processor arifact from Indy
+MAPSTRUCT_VERSION=$(ls $CEQ_MRRC_REPOSITORY/org/mapstruct/mapstruct)
+if [ ! -f "$LOCAL_REPO/org/mapstruct/mapstruct-processor/$MAPSTRUCT_VERSION/mapstruct-processor-$MAPSTRUCT_VERSION.jar" ]; then
+    mkdir -p $LOCAL_REPO/org/mapstruct/mapstruct-processor/$MAPSTRUCT_VERSION
+    curl $INDY_BASE_URL/org/mapstruct/mapstruct-processor/$MAPSTRUCT_VERSION/mapstruct-processor-$MAPSTRUCT_VERSION.jar > $LOCAL_REPO/org/mapstruct/mapstruct-processor/$MAPSTRUCT_VERSION/mapstruct-processor-$MAPSTRUCT_VERSION.jar
+    curl $INDY_BASE_URL/org/mapstruct/mapstruct-processor/$MAPSTRUCT_VERSION/mapstruct-processor-$MAPSTRUCT_VERSION.pom > $LOCAL_REPO/org/mapstruct/mapstruct-processor/$MAPSTRUCT_VERSION/mapstruct-processor-$MAPSTRUCT_VERSION.pom
 fi
 
 # get the Camel Quarkus version from the source tree
@@ -132,12 +136,25 @@ mvn clean install -Plocal-mrrc \
 
 # Run the tests
 # product/integration-tests-product/pom.xml
-#
-
 mvn clean test -Plocal-mrrc -fae \
   -s $SETTINGS \
-  -f integration-tests/jms-artemis-client/pom.xml \
-  -Denforcer.skip \
+  -f product/integration-tests-product/pom.xml \
+  -DskipTests \
+  -Dcq.prod-artifacts.skip \
+  -DnoVirtualDependencies \
+  -Dquarkus.platform.group-id=$QUARKUS_BOM_GROUP_ID \
+  -Dquarkus.platform.artifact-id=$QUARKUS_BOM_ARTIFACT_ID \
+  -Dquarkus.platform.version=$QUARKUS_BOM_VERSION \
+  -Dcamel-quarkus.platform.group-id=$CQ_BOM_GROUP_ID \
+  -Dcamel-quarkus.platform.artifact-id=$CQ_BOM_ARTIFACT_ID \
+  -Dcamel-quarkus.platform.version=$CQ_BOM_VERSION \
+  -Dcamel-quarkus.version=$CQ_VERSION
+
+# Build the superapp
+mvn clean test -Plocal-mrrc -fae \
+  -s $SETTINGS \
+  -f product/camel-quarkus-superapp/pom.xml \
+  -DskipTests \
   -Dcq.prod-artifacts.skip \
   -DnoVirtualDependencies \
   -Dquarkus.platform.group-id=$QUARKUS_BOM_GROUP_ID \
